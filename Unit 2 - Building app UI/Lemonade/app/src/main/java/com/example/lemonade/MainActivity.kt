65b44eee67552484d500d716448f4144028548be
Active_Nebula <vedanshparganiha108@gmail.com>
2025-10-20 01:42:18 +0530

    docs: Approach2
    Using two mutable state objects, "stage" and "lemonSqueeze".
    
    Stage - tracks which part of the process we are in.
    lemonSqueeze - counts the number of times lemon is squeezed.
    
    Using these two, we will assign values to the UI elements in  "LemonadeUI", a composable function that defines all our UI.
    I have yet to define all UI elements.

--- Unit 2 - Building app UI/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Unit 2 - Building app UI/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,14 +51,75 @@ fun DigitalLemonadeApp() {
 
 @Composable
 fun MakingLemonade(modifier: Modifier = Modifier) {
-    val stage by remember {mutableStateOf( 1 )}
-    val
+    var stage by remember {mutableStateOf( 1 )}
+    var lemonSqueeze by remember {mutableStateOf(2)}
 
+    when(stage) {
+        1 -> {
+            LemonadeUI(
+                imageID = painterResource(R.drawable.lemon_tree),
+                contentDescription = stringResource(R.string.Lemon_Tree),
+                textID = stringResource(R.string.Step1),
+                imageOnClick = {
+                    stage = 2
+                    lemonSqueeze = (2..4).random()
+                }
+            )
+        }
 
+        2 -> {
+            LemonadeUI(
+                imageID = painterResource(R.drawable.lemon_squeeze),
+                contentDescription = stringResource(R.string.Lemon),
+                textID = stringResource(R.string.Step2),
+                imageOnClick = {
+                    lemonSqueeze--
+                    if(lemonSqueeze == 0) {
+                        stage = 3
+                    }
+                }
+            )
+        }
+
+        3 -> {
+            LemonadeUI(
+                imageID = painterResource(R.drawable.lemon_drink),
+                contentDescription = stringResource(R.string.Glass_of_Lemonade),
+                textID = stringResource(R.string.Step3),
+                imageOnClick = {
+                    stage = 4
+                }
+            )
+        }
+
+        4 -> {
+            LemonadeUI(
+                imageID = painterResource(R.drawable.lemon_restart),
+                contentDescription = stringResource(R.string.Empty_Glass),
+                textID = stringResource(R.string.Step4),
+                imageOnClick = {
+                    stage = 1
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun LemonadeUI (
+    imageID: Painter,
+    contentDescription: Any,
+    textID: String,
+    imageOnClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = stringResource(R.string.app_name)
 
+        )
     }
 }
